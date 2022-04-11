75ea5a3035e5f0f056efbed93874b97e78532b94
Tharki-God <73281112+Tharki-God@users.noreply.github.com>
2022-04-12 05:25:58 +0530

    Create FakeNitro.plugin.js

---
+++ FakeNitro.plugin.js
@@ -0,0 +1,107 @@
+/**
+ * @name FakeNitro
+ * @author Ahlawat
+ * @authorId 887483349369765930
+ * @version 0.2
+ * @description Just enables nitro features but only client side features work (In Short Just Streaming 1080p 60FPS)
+ * @website https://wife-ruby.ml
+ * @source https://github.com/Tharki-God/BetterDiscordPlugins
+ * @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/FakeNitro.plugin.js
+ */
+module.exports = (() => {
+	const config = {
+		info: {
+			name: "FakeNitro",
+			authors: [
+				{
+					name: "Ahlawat",
+					discord_id: "887483349369765930",
+					github_username: "Tharki-God",
+				},
+			],
+			version: "0.2",
+			description:
+				"Just enables nitro features but only client side features work (In Short Just Streaming 1080p 60FPS)",
+			github: "https://github.com/Tharki-God/BetterDiscordPlugins",
+			github_raw:
+				"https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/FakeNitro.plugin.js",
+		},
+		main: ".plugin.js",
+	};
+
+	return !global.ZeresPluginLibrary
+		? class {
+				constructor() {
+					this._config = config;
+				}
+				getName() {
+					return config.info.name;
+				}
+				getAuthor() {
+					return config.info.authors.map((a) => a.name).join(", ");
+				}
+				getDescription() {
+					return config.info.description;
+				}
+				getVersion() {
+					return config.info.version;
+				}
+				load() {
+					BdApi.showConfirmationModal(
+						"Library Missing",
+						`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+						{
+							confirmText: "Download Now",
+							cancelText: "Cancel",
+							onConfirm: () => {
+								require("request").get(
+									"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+									async (error, response, body) => {
+										if (error)
+											return require("electron").shell.openExternal(
+												"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+											);
+										await new Promise((r) =>
+											require("fs").writeFile(
+												require("path").join(
+													BdApi.Plugins.folder,
+													"0PluginLibrary.plugin.js"
+												),
+												body,
+												r
+											)
+										);
+									}
+								);
+							},
+						}
+					);
+				}
+				start() {}
+				stop() {}
+		  }
+		: (([Plugin, Api]) => {
+				const plugin = (Plugin, Api) => {
+					const { Patcher, DiscordModules, Settings, PluginUtilities } = Api;
+					return class NitroEmoteAndScreenShareBypass extends Plugin {
+										
+
+						onStart() {
+								const user = ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser();
+								if (!user) return;								
+								this.org = user.premiumType;
+								user.premiumType = 2;
+						}
+
+
+						onStop() {
+								const user = ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser();
+								if (!user) return;
+								user.premiumType = this.org;
+							}
+					};
+				};
+				return plugin(Plugin, Api);
+		  })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/
