17adfa17cb4aa5d5223c108e48625fc8f67eb850
Tharki-God <73281112+Tharki-God@users.noreply.github.com>
2022-05-12 04:31:42 +0530

    Initial release

--- NSFW-Bypass.plugin.js
+++ NSFW-Bypass.plugin.js
@@ -1,13 +1,35 @@
 /**
- * @name NSFW-Bypass
- * @author Ahlawat
- * @authorId 887483349369765930
- * @version 0.1.3
- * @description Bypass nsfw age restriction
- * @website https://wife-ruby.ml
- * @source https://github.com/Tharki-God/BetterDiscordPlugins
- * @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/NSFW-Bypass.plugin.js
- */
+	* @name NSFW-Bypass
+	* @author Ahlawat
+	* @authorId 887483349369765930
+	* @version 1.1.0
+	* @invite SgKSKyh9gY
+	* @description Bypass nsfw age restriction
+	* @website https://wife-ruby.ml
+	* @source https://github.com/Tharki-God/BetterDiscordPlugins
+	* @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/NSFW-Bypass.plugin.js
+*/
+/*@cc_on
+	@if (@_jscript)	
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+	shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+	shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+	fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+	// Show the user where to put plugins in the future
+	shell.Exec("explorer " + pathPlugins);
+	shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
+@else@*/
 module.exports = (() => {
 	const config = {
 		info: {
@@ -19,86 +41,104 @@ module.exports = (() => {
 					github_username: "Tharki-God",
 				},
 			],
-			version: "0.1.3",
+			version: "1.1.0",
 			description:
-				"Bypass nsfw age restriction",
+			"Bypass nsfw age restriction",
 			github: "https://github.com/Tharki-God/BetterDiscordPlugins",
 			github_raw:
-				"https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/NSFW-Bypass.plugin.js",
+			"https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/NSFW-Bypass.plugin.js",
 		},
+		changelog: [
+			{
+				title: "v0.1.3",
+				items: [
+					"Fixed missing library modal"
+				]
+			},
+			{
+				title: "Initial Release v1.1.0",
+				items: [
+					"This is the initial release of the plugin :)",
+					"Don't you look at naughty stuff (*￣3￣)╭"
+				]
+			}
+		],
 		main: "NSFW-Bypass.plugin.js",
-	};
-
+	};	
 	return !global.ZeresPluginLibrary
-		? class {
-			constructor() {
-				this._config = config;
-			}
-			getName() {
-				return config.info.name;
-			}
-			getAuthor() {
-				return config.info.authors.map((a) => a.name).join(", ");
-			}
-			getDescription() {
-				return config.info.description;
-			}
-			getVersion() {
-				return config.info.version;
-			}
-			load() {
-
-				BdApi.showConfirmationModal(
-					"Library Missing",
-					`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
-					{
-						confirmText: "Download Now",
-						cancelText: "Cancel",
-						onConfirm: () => {
-							require("request").get(
-								"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-								async (error, response, body) => {
-									if (error)
-										return require("electron").shell.openExternal(
-											"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
-										);
-									await new Promise((r) =>
-										require("fs").writeFile(
-											require("path").join(
-												BdApi.Plugins.folder,
-												"0PluginLibrary.plugin.js"
-											),
-											body,
-											r
-										)
-									);
-								}
-							);
-						},
-					}
-				);
-			}
-			start() { }
-			stop() { }
+	? class {
+		constructor() {
+			this._config = config;
 		}
-		: (([Plugin, Api]) => {
-			const plugin = (Plugin, Api) => {
-				const { Patcher, DiscordModules, Settings, PluginUtilities } = Api;
-				return class NSFWBypass extends Plugin {
-					onStart() {
-						const user = ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser();
-						if (!user) return;						
-						this.orginal = user.nsfwAllowed;
-						user.nsfwAllowed = true;
-					}
-					onStop() {
-						const user = ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser();
-						if (!user) return;
-						user.nsfwAllowed = this.orginal;
-					}
-				};
+		getName() {
+			return config.info.name;
+		}
+		getAuthor() {
+			return config.info.authors.map((a) => a.name).join(", ");
+		}
+		getDescription() {
+			return config.info.description;
+		}
+		getVersion() {
+			return config.info.version;
+		}
+		load() {
+			try {
+				global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
+			}
+			catch (err) {
+				console.error(this.getName(), "Plugin Updater could not be reached.", err);
+			}
+			BdApi.showConfirmationModal(
+				"Library Missing",
+				`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+				{
+					confirmText: "Download Now",
+					cancelText: "Cancel",
+					onConfirm: () => {
+						require("request").get(
+							"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+							async (error, response, body) => {
+								if (error)
+								return require("electron").shell.openExternal(
+									"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+								);
+								await new Promise((r) =>
+									require("fs").writeFile(
+										require("path").join(
+											BdApi.Plugins.folder,
+											"0PluginLibrary.plugin.js"
+										),
+										body,
+										r
+									)
+								);
+							}
+						);
+					},
+				}
+			);
+		}
+		start() { }
+		stop() { }
+	}
+	: (([Plugin]) => {
+		const plugin = (Plugin) => {
+			return class NSFWBypass extends Plugin {
+				onStart() {
+					const user = ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser();
+					if (!user) return;						
+					this.orginal = user.nsfwAllowed;
+					user.nsfwAllowed = true;
+				}
+				onStop() {
+					const user = ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser();
+					if (!user) return;
+					user.nsfwAllowed = this.orginal;
+				}
 			};
-			return plugin(Plugin, Api);
-		})(global.ZeresPluginLibrary.buildPlugin(config));
+		};
+		return plugin(Plugin);
+	})(global.ZeresPluginLibrary.buildPlugin(config));
 })();
 /*@end@*/
