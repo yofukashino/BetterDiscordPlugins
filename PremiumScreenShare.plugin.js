f6b134edb753e1c31e8f9b82edf18bc11f50ac2f
Tharki-God <73281112+Tharki-God@users.noreply.github.com>
2022-05-12 06:12:11 +0530

    initial release

---
+++ PremiumScreenShare.plugin.js
@@ -0,0 +1,155 @@
+/**
+	* @name PremiumScreenShare
+	* @author Ahlawat
+	* @authorId 887483349369765930
+	* @version 1.0.0
+	* @invite SgKSKyh9gY
+	* @description Enables 1080p 60FPS Stream on Discord without Nitro
+	* @website https://wife-ruby.ml
+	* @source https://github.com/Tharki-God/BetterDiscordPlugins/blob/master/PremiumScreenShare
+	* @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/PremiumScreenShare.plugin.js
+*/
+/*@cc_on
+	@if (@_jscript)	
+	// Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+	shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+	shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+	fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+	// Show the user where to put plugins in the future
+	shell.Exec("explorer " + pathPlugins);
+	shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
+@else@*/
+module.exports = (() => {
+	const config = {
+		info: {
+			name: "PremiumScreenShare",
+			authors: [
+				{
+					name: "Ahlawat",
+					discord_id: "887483349369765930",
+					github_username: "Tharki-God",
+				},
+			],
+			version: "0.0.5",
+			description:
+			"Enables 1080p 60FPS Stream on Discord without Nitro",
+			github: "https://github.com/Tharki-God/BetterDiscordPlugins",
+			github_raw:
+			"https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/PremiumScreenShare.plugin.js",
+		},
+		changelog: [
+			{
+				title: "v0.0.1",
+				items: [
+					"Idea in mind"
+				]
+			},
+			{
+				title: "v0.0.5",
+				items: [
+					"Base Model"
+				]
+			},
+			{
+				title: "Initial Release v1.0.0",
+				items: [
+					"This is the initial release of the plugin :)",
+					"Stream those tiddies real nice (╹ڡ╹ )"
+				]
+			}
+		],
+		main: "PremiumScreenShare.plugin.js",
+	};	
+	return !global.ZeresPluginLibrary
+	? class {
+		constructor() {
+			this._config = config;
+		}
+		getName() {
+			return config.info.name;
+		}
+		getAuthor() {
+			return config.info.authors.map((a) => a.name).join(", ");
+		}
+		getDescription() {
+			return config.info.description;
+		}
+		getVersion() {
+			return config.info.version;
+		}
+		load() {
+			try {
+				global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
+			}
+			catch (err) {
+				console.error(this.getName(), "Plugin Updater could not be reached.", err);
+			}
+			BdApi.showConfirmationModal(
+				"Library Missing",
+				`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+				{
+					confirmText: "Download Now",
+					cancelText: "Cancel",
+					onConfirm: () => {
+						require("request").get(
+							"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+							async (error, response, body) => {
+								if (error)
+								return require("electron").shell.openExternal(
+									"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+								);
+								await new Promise((r) =>
+									require("fs").writeFile(
+										require("path").join(
+											BdApi.Plugins.folder,
+											"0PluginLibrary.plugin.js"
+										),
+										body,
+										r
+									)
+								);
+							}
+						);
+					},
+				}
+			);
+		}
+		start() { }
+		stop() { }
+	}
+	: (([Plugin]) => {
+		const plugin = (Plugin) => {	
+			const Lodash = window._;
+			const Stream = BdApi.findModuleByProps("ApplicationStreamFPSButtons");
+			const requirements = Stream.ApplicationStreamSettingRequirements;
+			return class PremiumScreenShare extends Plugin {
+				onStart() {				
+					this.original = Lodash.cloneDeep(requirements);
+					for (let i = 0; i < requirements.length; i++) {
+						for (const key in requirements[i]) {
+							if (!~['resolution', 'fps'].indexOf(key)) {
+								delete requirements[i][key];
+							}
+						}
+					};
+				}
+				onStop() {					
+					Stream.ApplicationStreamSettingRequirements = this.original;
+				}
+			};
+		};
+		return plugin(Plugin);
+	})(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/
