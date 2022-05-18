6572762e8621ed057ed2eadf85df41eb16f2a4b2
Tharki-God <73281112+Tharki-God@users.noreply.github.com>
2022-05-18 19:50:14 +0530

    Update StarWars.plugin.js

--- StarWars.plugin.js
+++ StarWars.plugin.js
@@ -2,7 +2,7 @@
 	* @name StarWars
 	* @author Ahlawat
 	* @authorId 887483349369765930
-	* @version 1.0.2
+	* @version 1.0.3
 	* @invite SgKSKyh9gY
 	* @description Sends Random star war gif
 	* @website https://wife-ruby.ml
@@ -41,7 +41,7 @@ module.exports = (() => {
 					github_username: "Tharki-God",
 				},
 			],
-			version: "1.0.2",
+			version: "1.0.3",
 			description:
 			"Sends Random star war gif",
 			github: "https://github.com/Tharki-God/BetterDiscordPlugins",
@@ -73,6 +73,12 @@ module.exports = (() => {
 				items: [
 					"Code Defractor",
 					"More Random"
+				]
+			},
+			{
+				title: "v1.0.3",
+				items: [
+					"Fixed Erros"
 				]
 			}
 		],
@@ -176,11 +182,15 @@ module.exports = (() => {
 			const commands = Commands;
 			let gif;			
 			return class StarWars extends Plugin {
+				randomize() {
+					return Math.floor(Math.random() * (45 - 0 + 1) + 0)
+				}
 				getGif() {
-					fetch('https://g.tenor.com/v1/random?q=star%20wars&key=ZVWM77CCK1QF').then(function (response) {
+					let randomizer = this.randomize();
+					fetch('https://g.tenor.com/v1/random?q=star%20wars&key=ZVWM77CCK1QF&limit=50').then(function (response) {
 						return response.json();
 						}).then(function (data) {
-						gif = Object.entries(data.results)[1];
+						gif = Object.entries(data.results)[randomizer][1];
 						}).catch(function (err) {
 						// There was an error
 						console.warn('Something went wrong.', err);
@@ -200,6 +210,7 @@ module.exports = (() => {
 							execute: (_, {
 								channel
 							}) => { this.getGif()	
+							console.log(gif)
 							let send = _[0].value;
 							try {
 								if (!send) {
@@ -234,4 +245,4 @@ module.exports = (() => {
 		return plugin(Plugin);
 	})(global.ZeresPluginLibrary.buildPlugin(config));
 })();
-/*@end@*/
+/*@end@*/														
