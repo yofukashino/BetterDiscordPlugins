3981eea67982cf76dfab21b227b66ae89b9b563f
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-05-29 05:44:14 +0530

    Update FakeDeafen.plugin.js

--- FakeDeafen.plugin.js
+++ FakeDeafen.plugin.js
@@ -83,6 +83,12 @@ module.exports = (() => {
 					"Refractor"
 				]
 			},
+			{
+				title: "v0.4.0",
+				items: [
+					"Library Handler"
+				]
+			},
 			
 		],
 		main: "FakeDeafen.plugin.js",
@@ -121,10 +127,13 @@ module.exports = (() => {
 						require("request").get(
 							"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
 							async (error, response, body) => {
-								if (error)
-								return require("electron").shell.openExternal(
-									"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
-								);
+								if (error) {
+									return BdApi.showConfirmationModal("Error Downloading",
+										[
+											"Library plugin download failed. Manually install plugin library from the link below.",
+											BdApi.React.createElement("a", { href: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", target: "_blank" }, "Plugin Link")
+										],
+									); }
 								await new Promise((r) =>
 									require("fs").writeFile(
 										require("path").join(
@@ -164,7 +173,7 @@ module.exports = (() => {
 										type: "warning",
 										icon: true,
 										timeout: 10000
-									});
+										});
 									BdApi.showToast(`Go Fuck yourself ✪ ω ✪`,								
 										{
 											type: "warning",
