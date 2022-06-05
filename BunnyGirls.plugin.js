91e0fb804b77497d0f1133cdfb8c0c8456f2f761
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-06-05 05:52:49 +0530

    Update BunnyGirls.plugin.js

--- BunnyGirls.plugin.js
+++ BunnyGirls.plugin.js
@@ -2,7 +2,7 @@
 	* @name BunnyGirls
 	* @author Ahlawat
 	* @authorId 887483349369765930
-	* @version 1.0.3
+	* @version 1.0.4
 	* @invite SgKSKyh9gY
 	* @description Sends Random Bunny Girl gif
 	* @website https://wife-ruby.ml
@@ -10,7 +10,7 @@
 	* @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/BunnyGirls.plugin.js
 */
 /*@cc_on
-	@if (@_jscript)	
+	@if (@_jscript)
 	// Offer to self-install for clueless users that try to run this directly.
 	var shell = WScript.CreateObject("WScript.Shell");
 	var fs = new ActiveXObject("Scripting.FileSystemObject");
@@ -31,217 +31,197 @@
 	WScript.Quit();
 @else@*/
 module.exports = (() => {
-	const config = {
-		info: {
-			name: "BunnyGirls",
-			authors: [
-				{
-					name: "Ahlawat",
-					discord_id: "887483349369765930",
-					github_username: "Tharki-God",
-				},
-			],
-			version: "1.0.3",
-			description:
-			"Sends Random Bunny Girl gif",
-			github: "https://github.com/Tharki-God/BetterDiscordPlugins",
-			github_raw:
-			"https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/BunnyGirls.plugin.js",
+    const config = {
+        info: {
+            name: "BunnyGirls",
+            authors: [{
+				name: "Ahlawat",
+				discord_id: "887483349369765930",
+				github_username: "Tharki-God",
+			},
+            ],
+            version: "1.0.4",
+            description:
+            "Sends Random Bunny Girl gif",
+            github: "https://github.com/Tharki-God/BetterDiscordPlugins",
+            github_raw:
+            "https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/BunnyGirls.plugin.js",
 		},
-		changelog: [
-			{
-				title: "v0.0.1",
-				items: [
-					"Idea in mind"
-				]
-			},
-			{
-				title: "v0.0.5",
-				items: [
-					"Base Model"
-				]
-			},
-			{
-				title: "Initial Release v1.0.0",
-				items: [
-					"This is the initial release of the plugin :)",
-					"Stream those tiddies real nice (╹ڡ╹ )"
-				]
-			},
-			{
-				title: "v1.0.2",
-				items: [
-					"Code Defractor",
-					"More Random"
-				]
-			},
-			{
-				title: "v1.0.3",
-				items: [
-					"Fixed Erros"
-				]
-			}
-		],
-		main: "BunnyGirls.plugin.js",
-	};	
-	return !global.ZeresPluginLibrary
+        changelog: [{
+			title: "v0.0.1",
+			items: [
+				"Idea in mind"
+			]
+            }, {
+			title: "v0.0.5",
+			items: [
+				"Base Model"
+			]
+            }, {
+			title: "Initial Release v1.0.0",
+			items: [
+				"This is the initial release of the plugin :)",
+				"Stream those tiddies real nice (╹ڡ╹ )"
+			]
+            }, {
+			title: "v1.0.2",
+			items: [
+				"Code Defractor",
+				"More Random"
+			]
+            }, {
+			title: "v1.0.3",
+			items: [
+				"Fixed Erros"
+			]
+		}
+        ],
+        main: "BunnyGirls.plugin.js",
+	};
+    return !global.ZeresPluginLibrary
 	? class {
-		constructor() {
-			this._config = config;
+        constructor() {
+            this._config = config;
 		}
-		getName() {
-			return config.info.name;
+        getName() {
+            return config.info.name;
 		}
-		getAuthor() {
-			return config.info.authors.map((a) => a.name).join(", ");
+        getAuthor() {
+            return config.info.authors.map((a) => a.name).join(", ");
 		}
-		getDescription() {
-			return config.info.description;
+        getDescription() {
+            return config.info.description;
 		}
-		getVersion() {
-			return config.info.version;
+        getVersion() {
+            return config.info.version;
 		}
-		load() {
-			try {
-				global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
+        load() {
+			
+            try {
+                global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
+				} catch (err) {
+                console.error(this.getName(), "Plugin Updater could not be reached.", err);
 			}
-			catch (err) {
-				console.error(this.getName(), "Plugin Updater could not be reached.", err);
-			}
-			BdApi.showConfirmationModal(
-				"Library Missing",
-				`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
-				{
+            BdApi.showConfirmationModal(
+                "Library Missing",
+				`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
 					confirmText: "Download Now",
 					cancelText: "Cancel",
 					onConfirm: () => {
 						require("request").get(
 							"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-							async (error, response, body) => {
-								if (error)
-								return require("electron").shell.openExternal(
-									"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
-								);
-								await new Promise((r) =>
-									require("fs").writeFile(
-										require("path").join(
-											BdApi.Plugins.folder,
-											"0PluginLibrary.plugin.js"
-										),
-										body,
-										r
-									)
-								);
-							}
-						);
+							async(error, response, body) => {
+								if (error) {
+									return BdApi.showConfirmationModal("Error Downloading",
+										[
+											"Library plugin download failed. Manually install plugin library from the link below.",
+											BdApi.React.createElement("a", { href: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", target: "_blank" }, "Plugin Link")
+										],
+									); }
+									await new Promise((r) =>
+										require("fs").writeFile(
+											require("path").join(
+												BdApi.Plugins.folder,
+											"0PluginLibrary.plugin.js"),
+											body,
+										r));
+							});
 					},
-				}
-			);
-		}
-		start() { }
-		stop() { }
-	}
-	: (([Plugin]) => {
-		const plugin = (Plugin) => {			
-			const DiscordCommands = BdApi.findModuleByProps("BUILT_IN_COMMANDS");
-			const DiscordCommandTypes = BdApi.findModuleByProps("ApplicationCommandType");
-			const Types = DiscordCommandTypes.ApplicationCommandType;
-			const OptionTypes = DiscordCommandTypes.ApplicationCommandOptionType;
-			const PermissionTypes = DiscordCommandTypes.ApplicationCommandPermissionType;
-			if (!DiscordCommands.BUILT_IN_SECTIONS["tharki"]) DiscordCommands.BUILT_IN_SECTIONS["tharki"] = {
-				icon: "https://cdn.discordapp.com/attachments/889198641775001670/975544544366051418/IMG_20220506_002009_copy.png",
-				id: "tharki",
-				name: "tharki",
-				type: 1
-			};
-			function registerCommand(caller, options) {
-				const cmd = Object.assign({}, options, {
-					__registerId: caller,
-					applicationId: "tharki",
-					type: Types.BOT,
-					target: 1
 				});
-				DiscordCommands.BUILT_IN_COMMANDS.push(cmd);
-				return () => {
-					const index = DiscordCommands.BUILT_IN_COMMANDS.indexOf(cmd);
-					if (index < 0) return false;
-					DiscordCommands.BUILT_IN_COMMANDS.splice(index, 1);
-				};
+		}
+        start() {}
+        stop() {}
+	}
+	: (([Plugin, Library]) => {
+        const {
+            WebpackModules
+		} = Library;
+		const DiscordCommands = WebpackModules.getByProps("BUILT_IN_COMMANDS");
+        return class BunnyGirls extends Plugin {
+            async getGif(boolean) {
+                let randomizer = Math.floor(Math.random() * (45 - 0 + 1) + 0);
+                let gif;
+                await fetch('https://g.tenor.com/v1/random?q=bunny%girls&key=ZVWM77CCK1QF&limit=50').then(function (response) {
+                    return response.json();
+					}).then(function (data) {
+                    const url = Object.entries(data.results)[randomizer][1];
+                    if (!boolean) {
+                        gif = {
+							image: {
+								url: url.media[0].gif.url,
+								proxyURL: url.media[0].gif.url,
+								width: url.media[0].gif.dims[0],
+								height: url.media[0].gif.dims[1]
+							}
+						}
+					} else
+					gif = url.itemurl
+					}).catch(function (err) {
+					// There was an error
+					console.warn('Something went wrong.', err);
+				});
+				return gif;
 			}
-			function unregisterAllCommands(caller) {
+			onStart() {				
+				DiscordCommands.BUILT_IN_COMMANDS.push({
+					__registerId: this.getName(),
+					applicationId: "-1",
+					name: "bunny girls",
+					description: "Sends Random Bunny Girl gif.",
+					id: (-1 -BdApi.findModuleByProps("BUILT_IN_COMMANDS").BUILT_IN_COMMANDS.length).toString(),
+					type: 1,
+					target: 1,
+					predicate: () => true,
+					execute: (_, {
+						channel
+					}) => {
+					let send = _[0].value;
+					try {
+						if (!send) {
+							this.getGif(false).then((gif) => {
+								BdApi.findModuleByProps('sendBotMessage').sendBotMessage(channel.id, "", [gif]);
+								}).catch((error) => {
+								console.error(error);
+							});
+							} else {
+							this.getGif(true).then((gif) => {
+								BdApi.findModuleByProps('sendMessage').sendMessage(channel.id, {
+									content: gif,
+									tts: false,
+									invalidEmojis: [],
+									validNonShortcutEmojis: []
+								}, undefined, {});
+								}).catch((error) => {
+								console.error(error);
+							});
+						}
+						} catch (error) {
+						console.error(error);
+					}
+					},
+					options: [{
+						description: "Weather you want to send this or not.",
+						displayDescription: "Weather you want to send this or not.",
+						displayName: "Send",
+						name: "Send",
+						required: true,
+						type: 5
+					}
+					]
+				});
+			}			
+			onStop() {
+				this.unregisterAllCommands(this.getName());
+			}
+			unregisterAllCommands(caller) {
 				let index = DiscordCommands.BUILT_IN_COMMANDS.findIndex((cmd => cmd.__registerId === caller));
 				while (index > -1) {
 					DiscordCommands.BUILT_IN_COMMANDS.splice(index, 1);
 					index = DiscordCommands.BUILT_IN_COMMANDS.findIndex((cmd => cmd.__registerId === caller));
 				}
 			}
-			const Commands = {
-				registerCommand,
-				unregisterAllCommands
-			};
-			const commands = Commands;
-			let gif;			
-			return class BunnyGirls extends Plugin {				
-				randomize() {
-					return Math.floor(Math.random() * (45 - 0 + 1) + 0)
-				}
-				getGif() {
-					let randomizer = this.randomize();
-					fetch('https://g.tenor.com/v1/random?q=bunny%girls&key=ZVWM77CCK1QF&limit=50').then(function (response) {
-						return response.json();
-						}).then(function (data) {
-						gif = Object.entries(data.results)[randomizer][1];
-						}).catch(function (err) {
-						// There was an error
-						console.warn('Something went wrong.', err);
-					});}
-					onStart()
-					{           this.getGif();			
-						const DiscordCommands = ZeresPluginLibrary.WebpackModules.getByProps("BUILT_IN_COMMANDS");
-						DiscordCommands.BUILT_IN_COMMANDS.push({
-							__registerId: this.getName(),
-							applicationId: "tharki",
-							name: "BunnyGirls",
-							description: "Sends Random Bunny Girl gif.",
-							id: "bunny-girls",
-							type: 1,
-							target: 1,
-							predicate: () => true,
-							execute: (_, {
-								channel
-							}) => { this.getGif()	
-							let send = _[0].value;
-							try {
-								if (!send) {
-									BdApi.findModuleByProps('sendBotMessage').sendBotMessage(channel.id,"",[{"image":{
-										"url": gif.media[0].gif.url,
-										"proxyURL": gif.media[0].gif.url,
-										"width":gif.media[0].gif.dims[0],
-										"height":gif.media[0].gif.dims[1]
-									},}])							
-									} else {
-									BdApi.findModuleByProps('sendMessage').sendMessage(channel.id, {content: gif.itemurl, tts: false, invalidEmojis: [], validNonShortcutEmojis: []}, undefined, {});
-								}
-								} catch (error) {
-								console.error(error);
-							}
-							},
-							options: [{
-								description: "Weather you want to send this or not.",
-								displayDescription: "Weather you want to send this or not.",
-								displayName: "Send",
-								name: "Send",
-								required: true,
-								type: 5
-							}]
-						});	
-					}
-					onStop() {					
-						commands.unregisterAllCommands(this.getName());
-					}
-			};
 		};
-		return plugin(Plugin);
+		return plugin(Plugin, Library);
 	})(global.ZeresPluginLibrary.buildPlugin(config));
 })();
 /*@end@*/
