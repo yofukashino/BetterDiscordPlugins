7db274272a5108a3f69db3e9ee1f62e6e3f30cac
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-06-05 21:24:28 +0530

    Update DiscordStatus.plugin.js

--- DiscordStatus.plugin.js
+++ DiscordStatus.plugin.js
@@ -2,15 +2,15 @@
 	* @name DiscordStatus
 	* @author Ahlawat
 	* @authorId 887483349369765930
-	* @version 1.0.2
+	* @version 1.0.5
 	* @invite SgKSKyh9gY
-	* @description Returns discord status from https://discordstatus.com
+	* @description Adds a slash command to get discord status from https://discordstatus.com
 	* @website https://wife-ruby.ml
 	* @source https://github.com/Tharki-God/BetterDiscordPlugins
 	* @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/DiscordStatus.plugin.js
 */
 /*@cc_on
-	@if (@_jscript)	
+	@if (@_jscript)
 	// Offer to self-install for clueless users that try to run this directly.
 	var shell = WScript.CreateObject("WScript.Shell");
 	var fs = new ActiveXObject("Scripting.FileSystemObject");
@@ -31,131 +31,124 @@
 	WScript.Quit();
 @else@*/
 module.exports = (() => {
-	const config = {
-		info: {
-			name: "DiscordStatus",
-			authors: [
-				{
-					name: "Ahlawat",
-					discord_id: "887483349369765930",
-					github_username: "Tharki-God",
-				},
-			],
-			version: "1.0.2",
-			description:
-			"Returns discord status from https://discordstatus.com",
-			github: "https://github.com/Tharki-God/BetterDiscordPlugins",
-			github_raw:
-			"https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/DiscordStatus.plugin.js",
+    const config = {
+        info: {
+            name: "DiscordStatus",
+            authors: [{
+				name: "Ahlawat",
+				discord_id: "887483349369765930",
+				github_username: "Tharki-God",
+			},
+            ],
+            version: "1.0.5",
+            description:
+            "Adds a slash command to get discord status from https://discordstatus.com",
+            github: "https://github.com/Tharki-God/BetterDiscordPlugins",
+            github_raw:
+            "https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/DiscordStatus.plugin.js",
 		},
-		changelog: [
-			{
-				title: "v0.0.1",
-				items: [
-					"Idea in mind"
-				]
-			},
-			{
-				title: "v0.0.5",
-				items: [
-					"Base Model"
-				]
-			},
-			{
-				title: "Initial Release v1.0.0",
-				items: [
-					"This is the initial release of the plugin :)",
-					"Get those stats nerd."
-				]
-			},
-			{
-				title: "v1.0.1",
-				items: [
-					"Reindent",
-				]
-			}
-		],
-		main: "DiscordStatus.plugin.js",
-	};	
-	return !global.ZeresPluginLibrary
+        changelog: [{
+			title: "v0.0.1",
+			items: [
+				"Idea in mind"
+			]
+            }, {
+			title: "v0.0.5",
+			items: [
+				"Base Model"
+			]
+            }, {
+			title: "Initial Release v1.0.0",
+			items: [
+				"This is the initial release of the plugin :)",
+				"Get those stats nerd （︶^︶）"
+			]
+            }, {
+			title: "v1.0.1",
+			items: [
+				"Reindent",
+			]
+            }, {
+			title: "v1.0.5",
+			items: [
+				"Fully working",
+			]
+		}
+        ],
+        main: "DiscordStatus.plugin.js",
+	};
+    return !global.ZeresPluginLibrary
 	? class {
-		constructor() {
-			this._config = config;
+        constructor() {
+            this._config = config;
 		}
-		getName() {
-			return config.info.name;
+        getName() {
+            return config.info.name;
 		}
-		getAuthor() {
-			return config.info.authors.map((a) => a.name).join(", ");
+        getAuthor() {
+            return config.info.authors.map((a) => a.name).join(", ");
 		}
-		getDescription() {
-			return config.info.description;
+        getDescription() {
+            return config.info.description;
 		}
-		getVersion() {
-			return config.info.version;
+        getVersion() {
+            return config.info.version;
 		}
-		load() {
-			try {
-				global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
+        load() {
+            try {
+                global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
+				} catch (err) {
+                console.error(this.getName(), "Plugin Updater could not be reached.", err);
 			}
-			catch (err) {
-				console.error(this.getName(), "Plugin Updater could not be reached.", err);
-			}
-			BdApi.showConfirmationModal(
-				"Library Missing",
-				`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
-				{
+            BdApi.showConfirmationModal(
+                "Library Missing",
+				`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
 					confirmText: "Download Now",
 					cancelText: "Cancel",
 					onConfirm: () => {
 						require("request").get(
 							"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-							async (error, response, body) => {
+							async(error, response, body) => {
 								if (error)
 								return require("electron").shell.openExternal(
-									"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
-								);
+								"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
 								await new Promise((r) =>
 									require("fs").writeFile(
 										require("path").join(
 											BdApi.Plugins.folder,
-											"0PluginLibrary.plugin.js"
-										),
+										"0PluginLibrary.plugin.js"),
 										body,
-										r
-									)
-								);
-							}
-						);
+									r));
+							});
 					},
-				}
-			);
+				});
 		}
-		start() { }
-		stop() { }
+        start() {}
+        stop() {}
 	}
 	: (([Plugin, Library]) => {
-		const {
+        const {
             WebpackModules
 		} = Library;
-		const DiscordCommands = BdApi.findModuleByProps("BUILT_IN_COMMANDS");
-		return class Status extends Plugin {			
-			async stats () {
-				let embed;
-				await fetch('https://discordstatus.com/api/v2/summary.json').then(function (response) {
-					return response.json();
+        const DiscordCommands = WebpackModules.getByProps("BUILT_IN_COMMANDS");
+		const sendBotMessage = WebpackModules.getByProps('sendBotMessage');
+        return class Status extends Plugin {
+            async stats() {
+                let embed;
+                await fetch('https://discordstatus.com/api/v2/summary.json').then(function (response) {
+                    return response.json();
 					}).then(function (data) {
-					const capitalize = (text) => text[0].toUpperCase() + text.slice(1);
-					embed =  {
-						type: 'rich',
-						title: data.status.description,
-						description: '[Discord Status](https://discordstatus.com/)\n' + '**Current Incident:**\n' + data.status.indicator,
+                    const capitalize = (text) => text[0].toUpperCase() + text.slice(1);
+                    embed = {
+                        type: 'rich',
+                        title: data.status.description,
+                        description: '[Discord Status](https://discordstatus.com/)\n' + '**Current Incident:**\n' + data.status.indicator,
 						color: "6577E6",
 						thumbnail: {
 							url: "https://media.discordapp.net/attachments/963485601045307412/978017538485153833/372108630_DISCORD_LOGO_400.gif",
 							proxyURL: "https://cdn.discordapp.com/attachments/963485601045307412/978017538485153833/372108630_DISCORD_LOGO_400.gif",
-							width:400,
-							height:400
+							width: 400,
+							height: 400
 						},
 						fields: data.components.map(component => ({
 							name: component.name,
@@ -165,41 +158,40 @@ module.exports = (() => {
 						timestamp: data.page.updated_at
 					}
 					}).catch(function (err) {
-					console.warn('Something went wrong.', err);
-				});					
-				return embed;					
-			}				
-			async onStart()
-			{  	
+                    console.warn('Something went wrong.', err);
+				});
+				return embed;
+			}
+			async onStart() {
 			DiscordCommands.BUILT_IN_COMMANDS.push({
-					__registerId: this.getName(),
-					applicationId: "-1",
-					name: "discord status",						
-					description: "Returns discord status from https://discordstatus.com",
-					id: (-1 -BdApi.findModuleByProps("BUILT_IN_COMMANDS").BUILT_IN_COMMANDS.length).toString(),
-					target: 1,
-					type: 1,
-					predicate: () => true,
-					execute: (_, {
+                    __registerId: this.getName(),
+                    applicationId: "-1",
+                    name: "discord status",
+                    description: "Returns discord status from https://discordstatus.com",
+                    id: (-1 - DiscordCommands.BUILT_IN_COMMANDS.length).toString(),
+                    target: 1,
+                    type: 1,
+                    predicate: () => true,
+                    execute: (_, {
 						channel
 					}) => {
 					this.stats().then((embed) => {
-						BdApi.findModuleByProps('sendBotMessage').sendBotMessage(channel.id,"",[embed]);
+						sendBotMessage.sendBotMessage(channel.id, "", [embed]);
 						}).catch((error) => {
 						console.error(error);
-					});						
-					},
-					options: []
-				});	
+					});
+                    },
+                    options: []
+				});
 			}
-			onStop() {					
+			onStop() {
 				this.unregisterAllCommands(this.getName());
 			}
 			unregisterAllCommands(caller) {
 				let index = DiscordCommands.BUILT_IN_COMMANDS.findIndex((cmd => cmd.__registerId === caller));
 				while (index > -1) {
-					DiscordCommands.BUILT_IN_COMMANDS.splice(index, 1);
-					index = DiscordCommands.BUILT_IN_COMMANDS.findIndex((cmd => cmd.__registerId === caller));
+                    DiscordCommands.BUILT_IN_COMMANDS.splice(index, 1);
+                    index = DiscordCommands.BUILT_IN_COMMANDS.findIndex((cmd => cmd.__registerId === caller));
 				}
 			}
 		};
