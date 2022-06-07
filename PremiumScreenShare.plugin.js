866ebf3046d11235b5eace0d727a0ed670086fa9
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-06-07 05:50:15 +0530

    Update PremiumScreenShare.plugin.js

--- PremiumScreenShare.plugin.js
+++ PremiumScreenShare.plugin.js
@@ -2,7 +2,7 @@
 	* @name PremiumScreenShare
 	* @author Ahlawat
 	* @authorId 887483349369765930
-	* @version 1.0.3
+	* @version 1.0.4
 	* @invite SgKSKyh9gY
 	* @description Enables 1080p 60FPS Stream on Discord without Nitro
 	* @website https://wife-ruby.ml
@@ -10,7 +10,7 @@
 	* @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/PremiumScreenShare.plugin.js
 */
 /*@cc_on
-	@if (@_jscript)	
+	@if (@_jscript)
 	// Offer to self-install for clueless users that try to run this directly.
 	var shell = WScript.CreateObject("WScript.Shell");
 	var fs = new ActiveXObject("Scripting.FileSystemObject");
@@ -31,132 +31,135 @@
 	WScript.Quit();
 @else@*/
 module.exports = (() => {
-	const config = {
-		info: {
-			name: "PremiumScreenShare",
-			authors: [
-				{
-					name: "Ahlawat",
-					discord_id: "887483349369765930",
-					github_username: "Tharki-God",
-				},
-			],
-			version: "1.0.3",
-			description:
-			"Enables 1080p 60FPS Stream on Discord without Nitro",
-			github: "https://github.com/Tharki-God/BetterDiscordPlugins",
-			github_raw:
-			"https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/PremiumScreenShare.plugin.js",
+    const config = {
+        info: {
+            name: "PremiumScreenShare",
+            authors: [{
+				name: "Ahlawat",
+				discord_id: "887483349369765930",
+				github_username: "Tharki-God",
+			},
+            ],
+            version: "1.0.4",
+            description:
+            "Enables 1080p 60FPS Stream on Discord without Nitro",
+            github: "https://github.com/Tharki-God/BetterDiscordPlugins",
+            github_raw:
+            "https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/PremiumScreenShare.plugin.js",
 		},
-		changelog: [
-			{
-				title: "v0.0.1",
-				items: [
-					"Idea in mind"
-				]
-			},
-			{
-				title: "v0.0.5",
-				items: [
-					"Base Model"
-				]
-			},
-			{
-				title: "Initial Release v1.0.0",
-				items: [
-					"This is the initial release of the plugin :)",
-					"Stream those tiddies real nice (╹ڡ╹ )"
-				]
-			},
-			{
-				title: "v1.0.1",
-				items: [
-					"Library Handler"
-				]
-			},
-			{
-				title: "v1.0.3",
-				items: [
-					"Refractor"
-				]
-			}
-		],
-		main: "PremiumScreenShare.plugin.js",
-	};	
-	return !global.ZeresPluginLibrary
+        changelog: [{
+			title: "v0.0.1",
+			items: [
+				"Idea in mind"
+			]
+            }, {
+			title: "v0.0.5",
+			items: [
+				"Base Model"
+			]
+            }, {
+			title: "Initial Release v1.0.0",
+			items: [
+				"This is the initial release of the plugin :)",
+				"Stream those tiddies real nice (╹ڡ╹ )"
+			]
+            }, {
+			title: "v1.0.1",
+			items: [
+				"Library Handler"
+			]
+            }, {
+			title: "v1.0.3",
+			items: [
+				"Refractor"
+			]
+            }, {
+			title: "v1.0.4",
+			items: [
+				"Module Name"
+			]
+		}
+        ],
+        main: "PremiumScreenShare.plugin.js",
+	};
+    return !global.ZeresPluginLibrary
 	? class {
-		constructor() {
-			this._config = config;
+        constructor() {
+            this._config = config;
 		}
-		getName() {
-			return config.info.name;
+        getName() {
+            return config.info.name;
 		}
-		getAuthor() {
-			return config.info.authors.map((a) => a.name).join(", ");
+        getAuthor() {
+            return config.info.authors.map((a) => a.name).join(", ");
 		}
-		getDescription() {
-			return config.info.description;
+        getDescription() {
+            return config.info.description;
 		}
-		getVersion() {
-			return config.info.version;
+        getVersion() {
+            return config.info.version;
 		}
-		load() {
-			try {
-				global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
+        load() {
+            try {
+                global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
+				} catch (err) {
+                console.error(this.getName(), "Plugin Updater could not be reached.", err);
 			}
-			catch (err) {
-				console.error(this.getName(), "Plugin Updater could not be reached.", err);
-			}
-			BdApi.showConfirmationModal(
-				"Library Missing",
-				`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
-				{
+            BdApi.showConfirmationModal(
+                "Library Missing",
+				`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
 					confirmText: "Download Now",
 					cancelText: "Cancel",
 					onConfirm: () => {
 						require("request").get(
 							"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-							async (error, response, body) => {
+							async(error, response, body) => {
 								if (error) {
 									return BdApi.showConfirmationModal("Error Downloading",
 										[
 											"Library plugin download failed. Manually install plugin library from the link below.",
-											BdApi.React.createElement("a", { href: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", target: "_blank" }, "Plugin Link")
-										],
-									); }
+											BdApi.React.createElement("a", {
+												href: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+												target: "_blank"
+											}, "Plugin Link")
+										], );
+								}
 								await new Promise((r) =>
 									require("fs").writeFile(
 										require("path").join(
 											BdApi.Plugins.folder,
-											"0PluginLibrary.plugin.js"
-										),
+										"0PluginLibrary.plugin.js"),
 										body,
-										r
-									)
-								);
-							}
-						);
+									r));
+							});
 					},
-				}
-			);
+				});
 		}
-		start() { }
-		stop() { }
+        start() {}
+        stop() {}
 	}
-	: (([Plugin, Library]) => {	
-	const {WebpackModules} = Library;
-			const Stream = WebpackModules.getByProps("ApplicationStreamFPSButtons");
-			const Original = Stream.ApplicationStreamSettingRequirements;
-			return class PremiumScreenShare extends Plugin {
-				onStart() {
-					Stream.ApplicationStreamSettingRequirements = Original.map(obj => {return  {resolution: obj.resolution, fps: obj.fps}});
-				}
-				onStop() {		
-					Stream.ApplicationStreamSettingRequirements = Original;
-				}
-			};
-
-		return plugin(Plugin, Library);
+	: (([Plugin, Library]) => {
+        const {
+            WebpackModules,
+            Patcher
+		} = Library;
+        const ApplicationStreamFPSButtons = WebpackModules.getByProps("ApplicationStreamFPSButtons");
+        const Original = ApplicationStreamFPSButtons.ApplicationStreamSettingRequirements;
+        return class PremiumScreenShare extends Plugin {
+            onStart() {
+                ApplicationStreamFPSButtons.ApplicationStreamSettingRequirements = Original.map(obj => {
+                    return {
+                        resolution: obj.resolution,
+                        fps: obj.fps
+					}
+				});
+			}
+            onStop() {
+                ApplicationStreamFPSButtons.ApplicationStreamSettingRequirements = Original;
+			}
+		};
+		
+        return plugin(Plugin, Library);
 	})(global.ZeresPluginLibrary.buildPlugin(config));
 })();
-/*@end@*/
+/*@end@*/	
