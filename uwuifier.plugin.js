90ac4564290778205507b05ea203b65b74c4d22f
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-06-12 02:58:27 +0530

    Update uwuifier.plugin.js

--- uwuifier.plugin.js
+++ uwuifier.plugin.js
@@ -2,10 +2,10 @@
 	* @name uwuifier
 	* @author Ahlawat
 	* @authorId 887483349369765930
-	* @version 1.0.0
+	* @version 1.0.1
 	* @invite SgKSKyh9gY
 	* @description Adds a slash command to uwuify the text you send.
-	* @website https://wife-ruby.ml
+	* @website https://tharki-god.github.io/
 	* @source https://github.com/Tharki-God/BetterDiscordPlugins
 	* @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/uwuifier.plugin.js
 */
@@ -40,7 +40,7 @@ module.exports = (() => {
 				github_username: "Tharki-God",
 			},
             ],
-            version: "1.0.0",
+            version: "1.0.1",
             description:
             "Adds a slash command to uwuify the text you send.",
             github: "https://github.com/Tharki-God/BetterDiscordPlugins",
