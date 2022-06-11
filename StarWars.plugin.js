7c6ef09ebb354f6ba1872627192d78b1aee12254
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-06-12 02:56:21 +0530

    Update StarWars.plugin.js

--- StarWars.plugin.js
+++ StarWars.plugin.js
@@ -2,10 +2,10 @@
 	* @name StarWars
 	* @author Ahlawat
 	* @authorId 887483349369765930
-	* @version 1.0.5
+	* @version 1.0.6
 	* @invite SgKSKyh9gY
 	* @description Adds a slash command to get send random star war gif
-	* @website https://wife-ruby.ml
+	* @website https://tharki-god.github.io/
 	* @source https://github.com/Tharki-God/BetterDiscordPlugins
 	* @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/StarWars.plugin.js
 */
@@ -41,7 +41,7 @@ module.exports = (() => {
 					github_username: "Tharki-God",
 				},
 			],
-			version: "1.0.5",
+			version: "1.0.6",
 			description:
 			"Adds a slash command to get send random star war gif",
 			github: "https://github.com/Tharki-God/BetterDiscordPlugins",
