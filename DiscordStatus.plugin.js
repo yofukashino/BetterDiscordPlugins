220c639db711736287dc5617fe1d6ba4894b5188
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-06-12 02:46:27 +0530

    Update DiscordStatus.plugin.js

--- DiscordStatus.plugin.js
+++ DiscordStatus.plugin.js
@@ -2,10 +2,10 @@
 	* @name DiscordStatus
 	* @author Ahlawat
 	* @authorId 887483349369765930
-	* @version 1.0.5
+	* @version 1.0.6
 	* @invite SgKSKyh9gY
 	* @description Adds a slash command to get discord status from https://discordstatus.com
-	* @website https://wife-ruby.ml
+	* @website https://tharki-god.github.io/
 	* @source https://github.com/Tharki-God/BetterDiscordPlugins
 	* @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/DiscordStatus.plugin.js
 */
@@ -40,7 +40,7 @@ module.exports = (() => {
 				github_username: "Tharki-God",
 			},
             ],
-            version: "1.0.5",
+            version: "1.0.6",
             description:
             "Adds a slash command to get discord status from https://discordstatus.com",
             github: "https://github.com/Tharki-God/BetterDiscordPlugins",
