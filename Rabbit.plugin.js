163cfa6b1b9aaa43b3341f76a17285b6ec7ce772
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-06-12 02:53:10 +0530

    Update Rabbit.plugin.js

--- Rabbit.plugin.js
+++ Rabbit.plugin.js
@@ -2,10 +2,10 @@
 	* @name Rabbit
 	* @author Ahlawat
 	* @authorId 887483349369765930
-	* @version 1.0.5
+	* @version 1.0.6
 	* @invite SgKSKyh9gY
 	* @description Adds a slash command to get send random Rabbit gif
-	* @website https://wife-ruby.ml
+	* @website https://tharki-god.github.io/
 	* @source https://github.com/Tharki-God/BetterDiscordPlugins
 	* @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/Rabbit.plugin.js
 */
@@ -41,7 +41,7 @@ module.exports = (() => {
 					github_username: "Tharki-God",
 				},
 			],
-			version: "1.0.5",
+			version: "1.0.6",
 			description:
 			"Adds a slash command to get send random rabbit gif",
 			github: "https://github.com/Tharki-God/BetterDiscordPlugins",
