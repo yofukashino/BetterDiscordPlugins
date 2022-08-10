010b1c06d20822d31c7d9c9087d192241004e541
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-08-10 22:39:38 +0530

    v1.2.1

--- FakeDeafen.plugin.js
+++ FakeDeafen.plugin.js
@@ -2,7 +2,7 @@
  * @name FakeDeafen
  * @author Ahlawat
  * @authorId 887483349369765930
- * @version 1.2.0
+ * @version 1.2.1
  * @invite SgKSKyh9gY
  * @description Fake your VC Status to Trick your Friends
  * @website https://tharki-god.github.io/
@@ -41,7 +41,7 @@ module.exports = (() => {
 			github_username: "Tharki-God",
 		  },
 		],
-		version: "1.2.0",
+		version: "1.2.1",
 		description: "Fake your VC Status to Trick your Friends",
 		github: "https://github.com/Tharki-God/BetterDiscordPlugins",
 		github_raw:
