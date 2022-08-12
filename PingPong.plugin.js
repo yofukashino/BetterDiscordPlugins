16db690f5962b1b7b032711e0c6c8ea6f96bf892
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-08-12 06:19:01 +0530

    rewrite

--- PingPong.plugin.js
+++ PingPong.plugin.js
@@ -1,14 +1,14 @@
 /**
-	* @name PingPong
-	* @author Kirai
-	* @authorId 887483349369765930
-	* @version 1.0.1
-	* @invite SgKSKyh9gY
-	* @description Randomize Ping Number.
-	* @website https://tharki-god.github.io/
-	* @source https://github.com/Tharki-God/BetterDiscordPlugins
-	* @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/Ping.plugin.js
-*/
+ * @name PingPong
+ * @author Kirai
+ * @authorId 887483349369765930
+ * @version 1.0.2
+ * @invite SgKSKyh9gY
+ * @description Randomize Ping Number.
+ * @website https://tharki-god.github.io/
+ * @source https://github.com/Tharki-God/BetterDiscordPlugins
+ * @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/Ping.plugin.js
+ */
 /*@cc_on
 	@if (@_jscript)
 	// Offer to self-install for clueless users that try to run this directly.
@@ -30,119 +30,138 @@
 	}
 	WScript.Quit();
 @else@*/
-module.exports = (_ => {
-    const config = {
-        info: {
-            name: "PingPong",
-            authors: [{
-                    name: "Kirai",
-                    discord_id: "872383230328832031",
-                    github_username: "HiddenKirai",
-                },
-            ],
-            version: "1.0.1",
-            description:
-            "Randomize Ping Number.",
-            github: "https://github.com/Tharki-God/BetterDiscordPlugins",
-            github_raw:
-            "https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/Ping.plugin.js",
+module.exports = ((_) => {
+	const config = {
+	  info: {
+		name: "PingPong",
+		authors: [
+		  {
+			name: "Kirai",
+			discord_id: "872383230328832031",
+			github_username: "HiddenKirai",
+		  },
+		],
+		version: "1.0.2",
+		description: "Randomize Ping Number.",
+		github: "https://github.com/Tharki-God/BetterDiscordPlugins",
+		github_raw:
+		  "https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/Ping.plugin.js",
+	  },
+	  changelog: [
+		{
+		  title: "v0.0.1",
+		  items: ["Idea in mind"],
 		},
-        changelog: [{
-			title: "v0.0.1",
-			items: [
-				"Idea in mind"
-			]
-            }, {
-			title: "v0.0.5",
-			items: [
-				"Base Model"
-			]
-            }, {
-			title: "Initial Release v1.0.0",
-			items: [
-				"This is the initial release of the plugin :)",
-				"I am useless (￣m￣）"
-			]
-		}
-        ],
-        main: "Ping.plugin.js",
+		{
+		  title: "v0.0.5",
+		  items: ["Base Model"],
+		},
+		{
+		  title: "Initial Release v1.0.0",
+		  items: [
+			"This is the initial release of the plugin :)",
+			"I am useless (￣m￣）",
+		  ],
+		},
+	  ],
+	  main: "Ping.plugin.js",
 	};
-    return !global.ZeresPluginLibrary
-	? class {
-        constructor() {
-            this._config = config;
+	return !global.ZeresPluginLibrary
+	  ? class {
+		  constructor() {
+			this._config = config;
+		  }
+		  getName() {
+			return config.info.name;
+		  }
+		  getAuthor() {
+			return config.info.authors.map((a) => a.name).join(", ");
+		  }
+		  getDescription() {
+			return config.info.description;
+		  }
+		  getVersion() {
+			return config.info.version;
+		  }
+		  load() {
+			BdApi.showConfirmationModal(
+			  "Library Missing",
+			  `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+			  {
+				confirmText: "Download Now",
+				cancelText: "Cancel",
+				onConfirm: () => {
+				  require("request").get(
+					"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+					async (error, response, body) => {
+					  if (error) {
+						return BdApi.showConfirmationModal("Error Downloading", [
+						  "Library plugin download failed. Manually install plugin library from the link below.",
+						  BdApi.React.createElement(
+							"a",
+							{
+							  href: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+							  target: "_blank",
+							},
+							"ZeresPluginLibrary"
+						  ),
+						]);
+					  }
+					  await new Promise((r) =>
+						require("fs").writeFile(
+						  require("path").join(
+							BdApi.Plugins.folder,
+							"0PluginLibrary.plugin.js"
+						  ),
+						  body,
+						  r
+						)
+					  );
+					}
+				  );
+				},
+			  }
+			);
+		  }
+		  start() {}
+		  stop() {}
 		}
-        getName() {
-            return config.info.name;
-		}
-        getAuthor() {
-            return config.info.authors.map((a) => a.name).join(", ");
-		}
-        getDescription() {
-            return config.info.description;
-		}
-        getVersion() {
-            return config.info.version;
-		}
-        load() {
-            try {
-                global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
-				} catch (err) {
-                console.error(this.getName(), "Plugin Updater could not be reached.", err);
+	  : (([Plugin, Library]) => {
+		  const { WebpackModules, Patcher } = Library;
+		  const randomNo = (min, max) =>
+			Math.floor(Math.random() * (max - min + 1) + min);
+		  const MentionCountStore = WebpackModules.getByProps("getMentionCount");
+		  return class Ping extends Plugin {
+			checkForUpdates() {
+			  try {
+				PluginUpdater.checkForUpdate(
+				  config.info.name,
+				  config.info.version,
+				  config.info.github_raw
+				);
+			  } catch (err) {
+				Logger.err("Plugin Updater could not be reached.", err);
+			  }
 			}
-            BdApi.showConfirmationModal(
-                "Library Missing",
-				`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
-					confirmText: "Download Now",
-					cancelText: "Cancel",
-					onConfirm: () => {
-						require("request").get(
-							"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-							async(error, response, body) => {
-								if (error) {
-									return BdApi.showConfirmationModal("Error Downloading",
-										[
-											"Library plugin download failed. Manually install plugin library from the link below.",
-											BdApi.React.createElement("a", {
-												href: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-												target: "_blank"
-											}, "Plugin Link")
-										], );
-								}
-								await new Promise((r) =>
-									require("fs").writeFile(
-										require("path").join(
-											BdApi.Plugins.folder,
-										"0PluginLibrary.plugin.js"),
-										body,
-									r));
-							});
-					},
-				});
-		}
-        start() {}
-        stop() {}
-	}
-	: (([Plugin, Library]) => {
-        const {
-            WebpackModules,
-            Patcher   
-		} = Library;
-   const num = () => {
-   return Math.floor(Math.random() * (9999 - 0 + 1) + 0);}
-const getMentionCount = WebpackModules.getByProps("getMentionCount");	
-        return class Ping extends Plugin {
-            onStart() {
-               Patcher.instead(getMentionCount, "getMentionCount", (_, args, res) => {
-				   return num();
-				   })
+			start() {
+			  this.checkForUpdates();
+			  this.patchPings();
 			}
-            onStop() {
-               Patcher.unpatchAll()
-			}       
-			
-		};
-        return plugin(Plugin, Library);
-	})(global.ZeresPluginLibrary.buildPlugin(config));
-})();
-/*@end@*/
+			patchPings() {
+			  Patcher.instead(
+				MentionCountStore,
+				"getMentionCount",
+				(_, args, res) => {
+				  return randomNo(0, 9000);
+				}
+			  );
+			}
+			onStop() {
+			  Patcher.unpatchAll();
+			}
+		  };
+		  return plugin(Plugin, Library);
+		})(global.ZeresPluginLibrary.buildPlugin(config));
+  })();
+  /*@end@*/
+  
