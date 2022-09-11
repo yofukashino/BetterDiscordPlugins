44b1655a40955003b36a7f65e73b458247581acd
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-09-11 15:40:09 +0530

    update

--- PingPong.plugin.js
+++ PingPong.plugin.js
@@ -2,33 +2,30 @@
  * @name PingPong
  * @author Kirai
  * @authorId 887483349369765930
- * @version 1.0.4
+ * @version 1.0.2
  * @invite SgKSKyh9gY
  * @description Randomize Ping Number.
  * @website https://tharki-god.github.io/
  * @source https://github.com/Tharki-God/BetterDiscordPlugins
- * @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/PingPong.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/Tharki-God/BetterDiscordPlugins/master/Ping.plugin.js
  */
 /*@cc_on
-	@if (@_jscript)
-	// Offer to self-install for clueless users that try to run this directly.
-	var shell = WScript.CreateObject("WScript.Shell");
-	var fs = new ActiveXObject("Scripting.FileSystemObject");
-	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
-	var pathSelf = WScript.ScriptFullName;
-	// Put the user at ease by Pinging them in the first person
-	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-	shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-	} else if (!fs.FolderExists(pathPlugins)) {
-	shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-	fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-	// Show the user where to put plugins in the future
-	shell.Exec("explorer " + pathPlugins);
-	shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-	}
-	WScript.Quit();
+@if (@_jscript)
+var shell = WScript.CreateObject("WScript.Shell");
+var fs = new ActiveXObject("Scripting.FileSystemObject");
+var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+var pathSelf = WScript.ScriptFullName;
+shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+} else if (!fs.FolderExists(pathPlugins)) {
+shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+} else if (shell.Popup("Should I move myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+fs.MoveFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)));
+shell.Exec("explorer " + pathPlugins);
+shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+}
+WScript.Quit();
 @else@*/
 module.exports = ((_) => {
   const config = {
@@ -41,7 +38,7 @@ module.exports = ((_) => {
           github_username: "HiddenKirai",
         },
       ],
-      version: "1.0.4",
+      version: "1.0.2",
       description: "Randomize Ping Number.",
       github: "https://github.com/Tharki-God/BetterDiscordPlugins",
       github_raw:
@@ -66,68 +63,62 @@ module.exports = ((_) => {
     ],
     main: "Ping.plugin.js",
   };
-  return !global.ZeresPluginLibrary
-    ? class {
-        constructor() {
-          this._config = config;
-        }
-        getName() {
-          return config.info.name;
-        }
-        getAuthor() {
-          return config.info.authors.map((a) => a.name).join(", ");
-        }
-        getDescription() {
-          return config.info.description;
-        }
-        getVersion() {
-          return config.info.version;
-        }
-        load() {
-          BdApi.showConfirmationModal(
-            "Library Missing",
-            `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
-            {
-              confirmText: "Download Now",
-              cancelText: "Cancel",
-              onConfirm: () => {
-                require("request").get(
-                  "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-                  async (error, response, body) => {
-                    if (error) {
-                      return BdApi.showConfirmationModal("Error Downloading", [
-                        "Library plugin download failed. Manually install plugin library from the link below.",
-                        BdApi.React.createElement(
-                          "a",
-                          {
-                            href: "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
-                            target: "_blank",
-                          },
-                          "ZeresPluginLibrary"
-                        ),
-                      ]);
-                    }
-                    await new Promise((r) =>
-                      require("fs").writeFile(
-                        require("path").join(
-                          BdApi.Plugins.folder,
-                          "0PluginLibrary.plugin.js"
-                        ),
-                        body,
-                        r
-                      )
-                    );
-                  }
-                );
-              },
+  return !window.hasOwnProperty("ZeresPluginLibrary")
+  ? class {
+      load() {
+        BdApi.showConfirmationModal(
+          "ZLib Missing",
+          `The library plugin (ZeresPluginLibrary) needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+          {
+            confirmText: "Download Now",
+            cancelText: "Cancel",
+            onConfirm: () => this.downloadZLib(),
+          }
+        );
+      }
+      async downloadZLib() {
+        const fs = require("fs");
+        const path = require("path");
+        const ZLib = await fetch(
+          "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js"
+        );
+        if (!ZLib.ok) return this.errorDownloadZLib();
+        const ZLibContent = await ZLib.text();
+        try {
+          await fs.writeFile(
+            path.join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"),
+            ZLibContent,
+            (err) => {
+              if (err) return this.errorDownloadZLib();
             }
           );
+        } catch (err) {
+          return this.errorDownloadZLib();
         }
-        start() {}
-        stop() {}
       }
-    : (([Plugin, Library]) => {
-        const { PluginUpdater, Logger, WebpackModules, Patcher } = Library;
+      errorDownloadZLib() {
+        const { shell } = require("electron");
+        BdApi.showConfirmationModal(
+          "Error Downloading",
+          [
+            `ZeresPluginLibrary download failed. Manually install plugin library from the link below.`,
+          ],
+          {
+            confirmText: "Download",
+            cancelText: "Cancel",
+            onConfirm: () => {
+              shell.openExternal(
+                "https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js"
+              );
+            },
+          }
+        );
+      }
+      start() {}
+      stop() {}
+    }
+  :  (([Plugin, Library]) => {
+        const { WebpackModules, PluginUpdater, Logger, Patcher } = Library;
         const randomNo = (min, max) =>
           Math.floor(Math.random() * (max - min + 1) + min);
         const MentionCountStore = WebpackModules.getByProps("getMentionCount");
