d8786224488a0859ca744860ce63f337c10ed625
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-09-29 20:25:15 +0530

    fixed for now

--- RejoinVC.plugin.js
+++ RejoinVC.plugin.js
@@ -152,22 +152,39 @@ module.exports = ((_) => {
           PluginUpdater,
           Logger,
           Utilities,
-          Settings: { SettingPanel, Slider, SettingGroup },
-          DiscordModules: { React, Dispatcher, ChannelActions },
+          ContextMenu,
+          Settings: { SettingPanel, Slider },
+          DiscordModules: { React, ChannelActions },
         } = Library;
         const { container } = WebpackModules.getByProps(
           "container",
           "usernameContainer"
         );
-        const PanelButton = WebpackModules.getByDisplayName("PanelButton");
+        const PanelButton = WebpackModules.getModule(
+          (m) => m.name == "m" && m.toString().includes("tooltipText")
+        );
+        const SliderComponent = WebpackModules.getModule((m) =>
+          m.render?.toString().includes("sliderContainer")
+        );
         const [Account] = ReactTools.getStateNodes(
           document.querySelector(`.${container}`)
         );
+        const Dispatcher = WebpackModules.getByProps("dispatch", "register");
         const CallJoin = (width, height) =>
-          React.createElement(WebpackModules.getByDisplayName("CallJoin"), {
-            width,
-            height,
-          });
+          React.createElement(
+            "svg",
+            {
+              viewBox: "0 0 24 24",
+              width,
+              height,
+            },
+            React.createElement("path", {
+              style: {
+                fill: "currentColor",
+              },
+              d: "M11 5V3C16.515 3 21 7.486 21 13H19C19 8.589 15.411 5 11 5ZM17 13H15C15 10.795 13.206 9 11 9V7C14.309 7 17 9.691 17 13ZM11 11V13H13C13 11.896 12.105 11 11 11ZM14 16H18C18.553 16 19 16.447 19 17V21C19 21.553 18.553 22 18 22H13C6.925 22 2 17.075 2 11V6C2 5.447 2.448 5 3 5H7C7.553 5 8 5.447 8 6V10C8 10.553 7.553 11 7 11H6C6.063 14.938 9 18 13 18V17C13 16.447 13.447 16 14 16Z",
+            })
+          );
         const CSS = `.withTagAsButton-OsgQ9L {
             min-width:0;
             }
@@ -210,53 +227,90 @@ module.exports = ((_) => {
             Dispatcher.unsubscribe("VOICE_CHANNEL_SELECT", this.PutButton);
           }
           PutButton(voice) {
-            if (voice.currentVoiceChannelId !== null) {
-              Patcher.unpatchAll();
-              Patcher.after(Account, "render", (_, args, res) => {
-                const {props: {children: [__, {props: {children}}]}} = res;                
-                children.unshift(
-                  React.createElement(PanelButton, {
-                    icon: () => CallJoin("20", "20"),
-                    tooltipText: "ReJoin VC",
-                    onClick: () => {
-                      Patcher.unpatchAll();
-                      ChannelActions.selectVoiceChannel(
-                        voice.currentVoiceChannelId
-                      );
+            if (voice?.currentVoiceChannelId == null) return;
+            Patcher.unpatchAll();
+            Patcher.after(Account, "render", (_, args, res) => {
+              const {
+                props: {
+                  children: [
+                    __,
+                    {
+                      props: { children },
                     },
-                  })
-                );
+                  ],
+                },
+              } = res;
+              const reJoinButton = React.createElement(PanelButton, {
+                icon: () => CallJoin("20", "20"),
+                tooltipText: "ReJoin VC",
+                onClick: () => {
+                  Patcher.unpatchAll();
+                  ChannelActions.selectVoiceChannel(
+                    voice.currentVoiceChannelId
+                  );
+                },
+                onContextMenu: (event) => {
+                  ContextMenu.openContextMenu(
+                    event,
+                    ContextMenu.buildMenu([
+                      {
+                        id: "show-time",
+                        label: "Show Time",
+                        type: "control",
+                        control: () =>
+                          React.createElement(SliderComponent, {
+                            value: this.settings["time"],
+                            initialValue: this.settings["time"],
+                            minValue: 5000,
+                            maxValue: 60000,
+                            renderValue: (value) => {
+                              const seconds = value / 1000;
+                              const minutes = value / 1000 / 60;
+                              return value < 60000
+                                ? `${seconds.toFixed(0)} secs`
+                                : `${minutes.toFixed(0)} min`;
+                            },
+                            onChange: (e) => {
+                              this.settings["time"] = e;
+                              this.saveSettings();
+                            },
+                          }),
+                      },
+                    ])
+                  );
+                },
               });
+              children.unshift(reJoinButton);
+            });
+            Account.forceUpdate();
+            clearTimeout(this.disappear);
+            this.disappear = setTimeout(() => {
+              Patcher.unpatchAll();
               Account.forceUpdate();
-              clearTimeout(this.disappear);
-              this.disappear = setTimeout(() => {
-                Patcher.unpatchAll();
-                Account.forceUpdate();
-              }, this.settings["time"]);
-            }
+            }, this.settings["time"]);
           }
           getSettingsPanel() {
             return SettingPanel.build(
-              this.saveSettings.bind(this),            
-                new Slider(
-                  "Show Time",
-                  "The Time to show the button after disconnect.",
-                  5000,
-                  60000,
-                  this.settings["time"],
-                  (e) => {
-                    this.settings["time"] = e;
+              this.saveSettings.bind(this),
+              new Slider(
+                "Show Time",
+                "The Time to show the button after disconnect.",
+                5000,
+                60000,
+                this.settings["time"],
+                (e) => {
+                  this.settings["time"] = e;
+                },
+                {
+                  onValueRender: (value) => {
+                    const seconds = value / 1000;
+                    const minutes = value / 1000 / 60;
+                    return value < 60000
+                      ? `${seconds.toFixed(0)} secs`
+                      : `${minutes.toFixed(0)} min`;
                   },
-                  {
-                    onValueRender: (value) => {
-                      const seconds = value / 1000;
-                      const minutes = value / 1000 / 60;
-                      return value < 60000
-                        ? `${seconds.toFixed(0)} secs`
-                        : `${minutes.toFixed(0)} min`;
-                    },
-                  }
-                )              
+                }
+              )
             );
           }
           saveSettings() {
