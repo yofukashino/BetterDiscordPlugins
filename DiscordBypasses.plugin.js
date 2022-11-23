1d43fa83b38ce645f53357abf4c33d510e879712
RobinRMC <github@robinmail.nl>
2022-11-23 09:21:08 +0100

    Correct DiscordBypasses.plugin.js

--- DiscordBypasses.plugin.js
+++ DiscordBypasses.plugin.js
@@ -457,8 +457,8 @@ module.exports = (() => {
                 }
               ),
               new Switch(
-                "Max Account Limit bypass",
-                "Removes the maximum account limit in discord's in-built account switcher.",
+                "Max. account limit bypass",
+                "Removes the maximum account limit in Discord's built-in account switcher.",
                 this.settings["maxAccounts"],
                 (e) => {
                   this.settings["maxAccounts"] = e;
