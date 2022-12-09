94887e611fca9af5fdaa0df10725af40a67a60e5
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-12-10 05:23:32 +0530

    update

--- DiscordBypasses.plugin.js
+++ DiscordBypasses.plugin.js
@@ -2,7 +2,7 @@
  * @name DiscordBypasses
  * @author Ahlawat
  * @authorId 1025214794766221384
- * @version 1.2.6
+ * @version 1.2.8
  * @invite SgKSKyh9gY
  * @description A collection of bypasses and utilities. Take a look in the plugin's settings for the features.
  * @website https://tharki-god.github.io/
@@ -38,7 +38,7 @@ module.exports = (() => {
           github_username: "Tharki-God",
         },
       ],
-      version: "1.2.6",
+      version: "1.2.8",
       description:
         "A collection of bypasses and utilities. Take a look in the plugin's settings for the features.",
       github: "https://github.com/Tharki-God/BetterDiscordPlugins",
@@ -101,6 +101,10 @@ module.exports = (() => {
       {
         title: "v1.2.6",
         items: ["Corrected text."],
+      },
+      {
+        title: "v1.2.7",
+        items: ["Custom stream preview now gets file locally."],
       }
     ],
     main: "DiscordBypasses.plugin.js",
@@ -166,12 +170,14 @@ module.exports = (() => {
         Logger,
         PluginUpdater,
         Patcher,
-        Settings: { SettingPanel, Switch, Textbox },
+        Toasts,
+        Settings: { SettingPanel, Switch, SettingField },
         DiscordModules: {
           CurrentUserIdle,
           UserStore,
           ExperimentsManager,
           ElectronModule,
+          React
         },
       } = Library;
       const Dispatcher = WebpackModules.getByProps(
@@ -189,57 +195,129 @@ module.exports = (() => {
         "getChannelPermissions"
       );
       const StreamPreviewStore = WebpackModules.getByProps("getPreviewURL");
-      const getBase64FromUrl = (url) => new Promise(async (resolve) => {
-        const data = await fetch(url);
-        const blob = await data.blob();
-        const reader = new FileReader();
-        reader.readAsDataURL(blob);
-        reader.onloadend = () => {
-          const base64data = reader.result;
-          resolve(base64data);
-        };
-      });
-      ;
-      const base64Size = (base64) => {
-        var stringLength = base64.length - `${base64.split(";base64,")[0]};base64,`.length;
-        var sizeInBytes =
-          4 * Math.ceil(stringLength / 3) * 0.5624896334383812;
-        return sizeInBytes / 1000;
-      };
-      const compressBase64 = (src, quality = 0.5) =>
-        new Promise((resolve) => {
-          const img = new Image();
-          img.src = src;
-          img.onload = () => {
-            const canvas = document.createElement("canvas");
-            canvas.width = img.width;
-            canvas.height = img.height;
-            canvas
-              .getContext("2d")
-              .drawImage(img, 0, 0, canvas.width, canvas.height);
-            resolve(canvas.toDataURL("image/jpeg", quality));
-          };
-        });
-      const getImageBase64 = (imageLink) =>
-        new Promise(async (resolve, reject) => {
-          if (!imageLink) reject("No image link was provided");
-          const image = await getBase64FromUrl(imageLink);
-          const imageSize = base64Size(image);
-          if (imageSize <= 200) resolve(image);
-          else resolve(await compressBase64(image, 200 / imageSize));
-        });
-      const isImage = (url) => {
-        return /^https?:\/\/.+\.(jpg|jpeg|png|webp)$/.test(url);
-      };
       const DeviceStore = WebpackModules.getModule((m) =>
         m?.Ai?.toString?.()?.includes("SPOTIFY_PROFILE_UPDATE")
       );
-      const isSpotifyPremium = WebpackModules.getModule((m) =>
+      const SpotifyPremiumCheck = WebpackModules.getModule((m) =>
         m?.yp?.toString?.()?.includes("spotify account is not premium")
       );
       const AccountSwitcherStrings = WebpackModules.getModule(
         (m) => m.Ip == "multiaccount_cta_tooltip_seen"
       );
+      class ClearButton extends React.Component {
+        render() {
+          const size = this.props.size || "16px";
+          return React.createElement("svg", {
+            className: this.props.className || "", fill: "currentColor", viewBox: "0 0 24 24", style: {
+              width: size, height: size,
+              padding: "0.5rem",
+              borderRadius: "0.3rem",
+              cursor: "pointer",
+              marginLeft: "70%",
+              marginRight: "auto",
+              marginTop: "1rem",
+            }, onClick: this.props.onClick
+          },
+            React.createElement("path", { d: "M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z" })
+          );
+        }
+      }
+      class ImagePickerWrapper extends React.Component {
+        constructor(props) {
+          super(props);
+          this.state = { img: this.props.file };
+        }
+        clear() {
+          this.setState({ img: "" });
+          this.props.onFileChange("");
+        }
+        getBase64(url) {
+          return new Promise(async (resolve) => {
+            const data = await fetch(url);
+            const blob = await data.blob();
+            const reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onloadend = () => {
+              resolve(reader.result);
+            };
+          });
+        }
+        render() {
+          return React.createElement("div", { style: { color: "white" } }, this.state.img ? React.createElement(ClearButton, {
+            className: "image-clear", onClick: this.clear.bind(this),
+          }) : "", React.createElement("div", null, React.createElement("input", {
+            ref: "file",
+            id: "actual-btn",
+            type: "file",
+            multiple: "false",
+            accept: "image/png, image/jpeg, image/webp,",
+            onChange: async (e) => {
+              const file = e.target.files[0]
+              if ((file.size / 1024) > 200) return Toasts.show(`File Must be under 200kb.`,
+                {
+                  icon: "https://raw.githubusercontent.com/Tharki-God/files-random-host/main/ic_fluent_error_circle_24_regular.png",
+                  timeout: 5000,
+                  type: "error",
+                });
+              const url = URL.createObjectURL(file);
+              const base64 = await this.getBase64(url);
+              this.props.onFileChange(base64);
+              this.setState({ img: base64 });
+            },
+            style: {
+              display: "none"
+            }
+          }), React.createElement("label", {
+            for: "actual-btn",
+            style: {
+
+              padding: "0.5rem",
+              borderRadius: "0.3rem",
+              cursor: "pointer",
+              marginLeft: "25%",
+              marginRight: "auto",
+              marginTop: "1rem",
+            }
+          }, this.state.img ? React.createElement("img", {
+            src: this.state.img,
+            style: {
+              maxWidth: "250px",
+              maxHeight: "250px",
+
+            }
+          }) : React.createElement("svg", {
+            width: "250",
+            height: "250",
+            viewBox: "0 0 24 24",
+            fill: "currentColor"
+          }, React.createElement("path", {
+            d: "M18.75 4C20.5449 4 22 5.45507 22 7.25V18.75C22 20.5449 20.5449 22 18.75 22H7.25C5.45507 22 4 20.5449 4 18.75V12.5019C4.47425 12.6996 4.97687 12.8428 5.50009 12.9236L5.5 18.75C5.5 18.9584 5.53643 19.1583 5.60326 19.3437L11.4258 13.643C12.2589 12.8273 13.5675 12.7885 14.4458 13.5266L14.5742 13.6431L20.3964 19.3447C20.4634 19.159 20.5 18.9588 20.5 18.75V7.25C20.5 6.2835 19.7165 5.5 18.75 5.5L12.9236 5.50009C12.8428 4.97687 12.6996 4.47425 12.5019 4H18.75ZM12.5588 14.644L12.4752 14.7148L6.66845 20.4011C6.8504 20.4651 7.04613 20.5 7.25 20.5H18.75C18.9535 20.5 19.1489 20.4653 19.3305 20.4014L13.5247 14.7148C13.2596 14.4553 12.8501 14.4316 12.5588 14.644ZM16.2521 7.5C17.4959 7.5 18.5042 8.50831 18.5042 9.75212C18.5042 10.9959 17.4959 12.0042 16.2521 12.0042C15.0083 12.0042 14 10.9959 14 9.75212C14 8.50831 15.0083 7.5 16.2521 7.5ZM6.5 1C9.53757 1 12 3.46243 12 6.5C12 9.53757 9.53757 12 6.5 12C3.46243 12 1 9.53757 1 6.5C1 3.46243 3.46243 1 6.5 1ZM16.2521 9C15.8367 9 15.5 9.33673 15.5 9.75212C15.5 10.1675 15.8367 10.5042 16.2521 10.5042C16.6675 10.5042 17.0042 10.1675 17.0042 9.75212C17.0042 9.33673 16.6675 9 16.2521 9ZM6.5 2.99923L6.41012 3.00729C6.20603 3.04433 6.0451 3.20527 6.00806 3.40936L6 3.49923L5.99965 5.99923L3.49765 6L3.40777 6.00806C3.20368 6.0451 3.04275 6.20603 3.00571 6.41012L2.99765 6.5L3.00571 6.58988C3.04275 6.79397 3.20368 6.9549 3.40777 6.99194L3.49765 7L6.00065 6.99923L6.00111 9.50348L6.00916 9.59336C6.04621 9.79745 6.20714 9.95839 6.41123 9.99543L6.50111 10.0035L6.59098 9.99543C6.79508 9.95839 6.95601 9.79745 6.99305 9.59336L7.00111 9.50348L7.00065 6.99923L9.50457 7L9.59444 6.99194C9.79853 6.9549 9.95947 6.79397 9.99651 6.58988L10.0046 6.5L9.99651 6.41012C9.95947 6.20603 9.79853 6.0451 9.59444 6.00806L9.50457 6L6.99965 5.99923L7 3.49923L6.99194 3.40936C6.9549 3.20527 6.79397 3.04433 6.58988 3.00729L6.5 2.99923Z"
+          })), React.createElement("div", {
+            for: "actual-btn",
+            style: {
+              color: "white",
+              padding: "0.5rem",
+              borderRadius: "0.3rem",
+              cursor: "pointer",
+              marginLeft: this.state.img ? "38%" : "42%",
+              marginRight: "auto",
+              marginTop: "1rem",
+            }
+          }, this.state.img ? "Change Image" : "Add Image"))));
+        }
+      }
+      class ImagePicker extends SettingField {
+        constructor(name, note, value, onChange, options = {}) {
+          const { disabled = false } = options;
+          super(name, note, onChange, ImagePickerWrapper, {
+            disabled: disabled,
+            file: value,
+            onFileChange: (e) => { this.onChange(e) },
+            onChange: () => null,
+          });
+        }
+      }
+
       const defaultSettings = {
         NSFW: !UserStore.getCurrentUser().nsfwAllowed,
         bandwidth: true,
@@ -309,12 +387,12 @@ module.exports = (() => {
           });
         }
         async patchStreamPreview() {
-          const replacePreviewWith = isImage(this.settings["fakePreview"])
-            ? await getImageBase64(this.settings["fakePreview"])
+          const replacePreviewWith = this.settings["fakePreview"] !== ""
+            ? this.settings["fakePreview"]
             : null;
           if (!replacePreviewWith)
             Logger.warn(
-              "No image link was provided, so no stream preview is being shown."
+              "No image was provided, so no stream preview is being shown."
             );
           Patcher.instead(
             ElectronModule,
@@ -367,7 +445,7 @@ module.exports = (() => {
               isPremium: true,
             });
           });
-          Patcher.instead(isSpotifyPremium, "Wo", () => true);
+          Patcher.instead(SpotifyPremiumCheck, "Wo", () => true);
         }
         patchGuildVerificationStore(toggle) {
           Object.defineProperty(DiscordConstants, "fDV", {
@@ -430,9 +508,9 @@ module.exports = (() => {
                 this.settings["preview"] = e;
               }
             ),
-            new Textbox(
-              "Image link",
-              "Link of an image to render as stream preview. (Must be under 200kb. If no image link is provided, no stream preview will be shown.)",
+            new ImagePicker(
+              "Custom Preview Image",
+              "Image to render as stream preview. (Must be under 200kb. If no image is provided, no stream preview will be shown.)",
               this.settings["fakePreview"],
               (e) => {
                 this.settings["fakePreview"] = e;
